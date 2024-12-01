402261bd27a858e0fc2b358eb85c437979e578a2
mohan@7667 <krishnamohansingh@605gmail.com>
2024-12-01 18:02:42 +0530

    first commit

---
+++ email.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>elite - Coworking Space in bangalore</title>
+    <link rel="icon" href="images/logo-light.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Coworking Space in bangalore" name="description" />
+    <meta content="" name="keywords" />
+    <meta content="" name="author" />
+    <!-- CSS Files
+    ================================================== -->
+    <link id="bootstrap" href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-grid" href="css/bootstrap-grid.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-reboot" href="css/bootstrap-reboot.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.theme.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.transitions.css" rel="stylesheet" type="text/css" />
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery.countdown.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <!-- color scheme -->
+    <link id="colors" href="css/colors/scheme-01.css" rel="stylesheet" type="text/css" />
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" />
+    <style>
+.am {
+    width: 81px;
+}
+.bm {
+    border-radius: 20px;
+    width: 100px;
+    height: 100px;
+
+}
+.cm > h4 {
+    text-transform: capitalize;
+    padding: 20px 1px 20px 70px;
+    font-size: 15px;
+}
+.de-box1.cm {
+    display: block !important;
+}
+.contactsec {
+    padding: 10px;
+    margin: 50px auto;
+    width: 100%;
+    height: auto;
+    background-color: #fff;
+    background: rgba(255,255,255,0.9);
+    border-radius: 15px;
+    position: relative;
+}
+</style>
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="preloader">
+            <div class="preloader1"></div>
+        </div>
+        <!-- header begin -->
+        <header class="transparent scroll-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="de-flex sm-pt10">
+                            <div class="de-flex-col">
+                                <div class="de-flex-col">
+                                    <!-- logo begin -->
+                                    <div id="logo">
+                                        <a href="index.html">
+                                            <img alt="" class="logo" src="images/logo.png" />
+                                            <img alt="" class="logo-2" src="images/logo.png" />
+                                        </a>
+                                    </div>
+                                    <!-- logo close -->
+                                </div>
+                                <div class="de-flex-col">
+                                </div>
+                            </div>
+                            <div class="de-flex-col header-col-mid">
+                                <!-- mainmenu begin -->
+                                <!--<ul id="mainmenu">-->
+                                <!--    <li>-->
+                                <!--        <a href="index.html">Home<span></span></a>-->
+                                <!--        <ul>-->
+                                <!--            <li><a href="index.html">Homepage 1</a></li>-->
+                                <!--            <li><a href="index-2.html">Homepage 2</a></li>-->
+                                <!--            <li><a href="index-3.html">Homepage 3</a></li>-->
+                                <!--        </ul>-->
+                                <!--    </li>-->
+                                <!--    <li>-->
+                                <!--        <a href="locations.html">Locations<span></span></a>-->
+                                <!--        <ul>-->
+                                <!--            <li><a href="locations.html">All Locations</a></li>-->
+                                <!--            <li><a href="search-result.html">Search Location</a></li>-->
+                                <!--            <li><a href="location-details.html">Location Details</a></li>-->
+                                <!--        </ul>-->
+                                <!--    </li>-->
+                                <!--    <li>-->
+                                <!--        <a href="#">Company<span></span></a>-->
+                                <!--        <ul>-->
+                                <!--            <li><a href="about-us.html">About Us</a></li>-->
+                                <!--            <li><a href="news.html">News</a></li>-->
+                                <!--            <li><a href="jobs.html">Jobs</a></li>-->
+                                <!--            <li><a href="contact.html">Contact</a></li>-->
+                                <!--        </ul>-->
+                                <!--    </li>-->
+                                <!--    <li>-->
+                                <!--        <a href="events.html">Events<span></span></a>-->
+                                <!--        <ul>-->
+                                <!--            <li><a href="events.html">Events</a></li>-->
+                                <!--            <li><a href="event-single.html">Event Details</a></li>-->
+                                <!--        </ul>-->
+                                <!--    </li>-->
+                                <!--    <li>-->
+                                <!--        <a href="#">Elements<span></span></a>-->
+                                <!--        <ul>-->
+                                <!--            <li><a href="icons-elegant.html">Elegant Icons</a></li>-->
+                                <!--            <li><a href="icons-etline.html">Etline Icons</a></li>-->
+                                <!--            <li><a href="icons-font-awesome.html">Font Awesome Icons</a></li>-->
+                                <!--            <li><a href="accordion.html">Accordion</a></li> -->
+                                <!--            <li><a href="alerts.html">Alerts</a></li>-->
+                                <!--            <li><a href="counters.html">Counters</a></li>-->
+                                <!--            <li><a href="modal.html">Modal</a></li>-->
+                                <!--            <li><a href="popover.html">Popover</a></li>-->
+                                <!--            <li><a href="pricing-table.html">Pricing Table</a></li>-->
+                                <!--            <li><a href="progress-bar.html">Progress Bar</a></li>-->
+                                <!--            <li><a href="tabs.html">Tabs</a></li>-->
+                                <!--            <li><a href="tooltips.html">Tooltips</a></li>-->
+                                <!--        </ul>-->
+                                <!--    </li>-->
+                                <!--</ul>                                -->
+                            </div>
+                            <div class="de-flex-col">
+                                <div class="menu_side_area">
+                                    <a href="login.html" class="ph"><span><i class="fa fa-phone fa-lg"></i>  +91-9113800972</span></a>
+                                    <!--<span id="menu-btn"></span>-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- header close -->
+        <!-- content begin -->
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+
+
+<?php 
+  if (isset($_REQUEST['email']))  { 
+  $admin_email = 'info@elitecoworks.com';
+ 
+  $cabins = $_REQUEST['cabins'];
+  $name = $_REQUEST['name'];
+  $email = $_REQUEST['email'];
+  $phone = $_REQUEST['phone'];
+  $company = $_REQUEST['company'];
+//   $location = $_REQUEST['location'];
+//   $seats = $_REQUEST['seats'];
+  
+  
+  $subject = "elite Enquiry Now";
+   
+  //echo "<pre>";
+  //print_r($_REQUEST);
+ //echo "</pre>";
+ //die('hhh'); 
+  $email_message .= "Cabins: ".$cabins."\n";
+  $email_message .= "Name: ".$name."\n";
+  $email_message .= "Email: ".$email."\n";
+  $email_message .= "Phone Number: ".$phone."\n";
+  $email_message .= "Msg: ".$company."\n";
+//   $email_message .= "Location: ".$location."\n";
+//   $email_message .= "Seats: ".$seats."\n";
+  mail($admin_email,"$subject",$email_message,"From:" . $email);//die("tt");
+ 
+  
+  }
+  //if "email" variable is not filled out, display the form
+  else  {
+?>
+
+  
+<?php
+  }
+?>
+
+
+
+<!---->
+   <section id="section-hero" style="background:#fff;height:500px; text-align:center;">
+<div class="tysec">
+<div class="container">
+<div class="contactsec">
+	<div class="tycont">
+    	<div class="tycont1">
+        
+        	<h1><i class="fa fa-check"></i></h1>
+            <h2><span>Thank You</span> for contacting with us. </br>We will reach you shortly.</h2>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</section>
+<!---->
+   <!-- content close -->
+        <a href="#" id="back-to-top"></a>
+        <!-- footer begin -->
+        <footer class="footer-light">
+        
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="de-flex">
+                                <div class="de-flex-col">
+                                    <a href="index.html">
+                                        <img alt="" class="f-logo" src="images/logo.png" /><span class="copy">&copy; Copyright 2022 - elite </span>
+                                    </a>
+                                </div>
+                                <div class="de-flex-col">
+                                    <div class="social-icons">
+                                        <a href="https://www.facebook.com/elitecoworks/"><i class="fa fa-facebook fa-lg"></i></a>
+                                    
+                                        <a href="https://www.linkedin.com/company/elitecoworks"><i class="fa fa-linkedin fa-lg"></i></a>
+                                 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+        
+    </div>
+
+    
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/OwlCarousel2Thumbs.min.js"></script>
+    <script src="js/validation.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/enquire.min.js"></script>
+    <script src="js/jquery.plugin.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script src="js/jquery.lazy.min.js"></script>
+    <script src="js/jquery.lazy.plugins.min.js"></script>
+    <script src="js/jquery.smartsticky.min.js"></script>
+    <script src="js/designesia.js"></script>
+
+    
+</body>
+
+</html>
